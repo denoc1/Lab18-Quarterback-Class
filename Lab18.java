98f497a0cf8e53d74c62f10b5b63f3f78ded10f7
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-12-01 04:51:06 +0000

    template

---
+++ Lab18.java
@@ -0,0 +1,20 @@
+public class Lab18{
+
+    public static void main(String[] args){
+
+        /** When you are finished with your constructors and print() method, you can delete
+         * the comment symbols and run the following code to test your code.
+            Quarterback brady   = new Quarterback("Tom Brady",   89214, 649, 212, 335, false);
+            Quarterback mahomes = new Quarterback("Patrick Mahomes", 32105, 245, 74, 111, true);
+            Quarterback allen   = new Quarterback("Josh Allen", 26434, 195, 84, 122, true);
+
+            Quarterback noName = new Quarterback();
+
+            mahomes.print();
+            allen.print();
+            brady.print();
+            noName.print();
+
+        */
+    }//end main
+}//end class
